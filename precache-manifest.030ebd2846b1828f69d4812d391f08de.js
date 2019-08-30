3838d21b0e58bebcb963a888c3d1630d5b5eaa87
jeanpier <monserrateluisje@gmail.com>
2019-08-30 11:16:12 -0400

    Updates

---
+++ precache-manifest.030ebd2846b1828f69d4812d391f08de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/Rick-and-Morty-API/static/media/logo.42d6efdb.png"
+  },
+  {
+    "revision": "eca68fa95dcfa77d76af",
+    "url": "/Rick-and-Morty-API/static/js/runtime~main.eca68fa9.js"
+  },
+  {
+    "revision": "a80048ce4e14ff68eb9a",
+    "url": "/Rick-and-Morty-API/static/js/main.a80048ce.chunk.js"
+  },
+  {
+    "revision": "41fd6703131b4d7e91ff",
+    "url": "/Rick-and-Morty-API/static/js/1.41fd6703.chunk.js"
+  },
+  {
+    "revision": "a80048ce4e14ff68eb9a",
+    "url": "/Rick-and-Morty-API/static/css/main.92b6e345.chunk.css"
+  },
+  {
+    "revision": "3cc26dacb788f1fea71023d3cd4c548e",
+    "url": "/Rick-and-Morty-API/index.html"
+  }
+];
